fbcdf2488355e2ca7fbb8ef02158b4c58907932f
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-06-23 10:59:29 -0300

    Closing connections

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,22 +10,27 @@ import db.DB;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			conn = DB.getConnection();
 			st = conn.createStatement();
 			rs = st.executeQuery("select * from department");
-			
+
 			while (rs.next()) {
 				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
 			}
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+		
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
 		}
 
 	}
